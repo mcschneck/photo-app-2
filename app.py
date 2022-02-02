9efb02c21485ab5a9ae299d443ca68143f3acfa3
Sarah <vanwars@gmail.com>
2022-02-02 11:38:59 -0600

    updates

--- app.py
+++ app.py
@@ -1,43 +1,45 @@
-'''
-Nice resources:
-  * https://picsum.photos/
-'''
-from flask import Flask, request, render_template
-import fake_data
-import json
+# load the environment variables:
+from dotenv import load_dotenv
+load_dotenv()
+
+from flask import Flask
+from flask_restful import Api
+import os
+from models import db, User
+from views import bookmarks, comments, followers, following, \
+    posts, profile, stories, suggestions, post_likes
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False    
+
+
+db.init_app(app)
+api = Api(app)
+
+# set logged in user
+with app.app_context():
+    app.current_user = User.query.filter_by(id=12).one()
+
+
+# Initialize routes for all of your API endpoints:
+bookmarks.initialize_routes(api)
+comments.initialize_routes(api)
+followers.initialize_routes(api)
+following.initialize_routes(api)
+posts.initialize_routes(api)
+post_likes.initialize_routes(api)
+profile.initialize_routes(api)
+stories.initialize_routes(api)
+suggestions.initialize_routes(api)
+
+# Server-side template for the homepage:
 @app.route('/')
 def home():
-    current_user = fake_data.generate_user()
-    return render_template(
-        'index.html', 
-        user=current_user,
-        posts=fake_data.generate_posts(n=8),
-        stories=fake_data.generate_stories(n=6),
-        suggestions=fake_data.generate_suggestions(n=7)
-    )
+    return 'Your API'
 
 
-@app.route('/api/feed')
-def get_feed():
-    n = request.args.get('limit') or 20
-    n = int(n)
-    print(fake_data.generate_posts(n))
-    return json.dumps(fake_data.generate_posts(n))
-
-@app.route('/api/stories')
-def get_stories():
-    n = request.args.get('limit') or 5
-    n = int(n)
-    return json.dumps(fake_data.generate_stories(n, width=50))
-
-@app.route('/api/suggestions')
-def get_suggestions():
-    n = request.args.get('limit') or 8
-    n = int(n)
-    return json.dumps(fake_data.generate_suggestions(n))
-
+# enables flask app to run using "python3 app.py"
 if __name__ == '__main__':
     app.run()
---
+++ views/bookmarks.py
@@ -0,0 +1,74 @@
+from flask import Response, request
+from flask_restful import Resource
+from models import Bookmark, db
+import json
+from . import can_view_post
+from my_decorators import handle_db_insert_error
+
+
+class BookmarksListEndpoint(Resource):
+    # 1. Lists all of the bookmarks
+    # 2. Create a new bookmark
+
+    def __init__(self, current_user):
+        self.current_user = current_user
+    
+    def get(self):
+        bookmarks = Bookmark.query.filter_by(
+            user_id=self.current_user.id).order_by('id').all()
+        bookmark_list_of_dictionaries = [
+            bookmark.to_dict() for bookmark in bookmarks
+        ]
+        return Response(json.dumps(bookmark_list_of_dictionaries), mimetype="application/json", status=200)
+
+
+    @handle_db_insert_error
+    def post(self):
+        '''
+        Goal: 
+            1. get the post_id from the request body
+            2. Check that the user is authorized to bookmark the post
+            3. Check that the post_id exists and is valid.
+            4. If 1, 2, & 3: insert to the database.
+            5. Return the new bookmarked post (and the bookmark id)
+               to the user as part of the response.
+        '''
+        # this is the data that the user sent us:
+        body = request.get_json()
+        post_id = body.get('post_id')
+        # to create a Bookmark, you need to pass it a user_id and a post_id
+        
+        bookmark = Bookmark(self.current_user.id, post_id)
+        # these two lines save ("commit") the new record to the database:
+        db.session.add(bookmark)
+        db.session.commit()
+        return Response(json.dumps(bookmark.to_dict()), mimetype="application/json", status=201)
+
+class BookmarkDetailEndpoint(Resource):
+
+    # 1. PATCH (updating), GET (individual bookmarks), DELETE individual bookmarks
+    # 2. Create a new bookmark
+
+    def __init__(self, current_user):
+        self.current_user = current_user
+    
+    def delete(self, id):
+        # Your code here
+        return Response(json.dumps({}), mimetype="application/json", status=200)
+
+
+
+def initialize_routes(api):
+    api.add_resource(
+        BookmarksListEndpoint, 
+        '/api/bookmarks', 
+        '/api/bookmarks/', 
+        resource_class_kwargs={'current_user': api.app.current_user}
+    )
+
+    api.add_resource(
+        BookmarkDetailEndpoint, 
+        '/api/bookmarks/<id>', 
+        '/api/bookmarks/<id>',
+        resource_class_kwargs={'current_user': api.app.current_user}
+    )
